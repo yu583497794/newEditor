1f99522c217d657a29b63f3dba89d40eef04a810
18507189651 <583497794@qq.com>
2019-10-23 15:47:04 +0800

    fix: 修复只有emoji时仍然会显示placeholder

--- src/components/MyEditor/plugins/placeholder/queries/isEmpty.ts
+++ src/components/MyEditor/plugins/placeholder/queries/isEmpty.ts
@@ -1,6 +1,6 @@
 import { Editor } from 'slate'
 const isEmpty = (editor: Editor) => {
-  return editor.value.startBlock.type === 'paragraph' && editor.value.document.text === ''
+  return editor.value.startBlock.type === 'paragraph' && editor.value.document.text === '' && editor.value.document.getInlines().size === 0
 } 
 
 export default isEmpty
